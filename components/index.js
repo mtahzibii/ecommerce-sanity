3da0a980821f3e09324a82795c6f8a3b5f69a538
tahzibi <m.r.tahzibi@gmail.com>
2023-01-28 13:57:22 +0330

    initial setup, sanity and hero banner

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import HeroBanner from './HeroBanner';
+import FooterBanner from './FooterBanner';
+import Layout from './Layout';
+import Product from './Product';
+import Cart from './Cart';
+
+export { Footer, HeroBanner, FooterBanner, Cart, Product, Layout, Navbar };
